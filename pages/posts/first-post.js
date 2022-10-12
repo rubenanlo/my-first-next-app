74e3209eacfb70323f59f25beafc7c8012a82d0c
Ruben Andino <rubenanlo@gmail.com>
2022-10-12 14:57:17 +0200

    checkpoint

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+// ADDING THIRD-PARTY JS --------->
+// import Script from "next/script";
+
 function FirstPost() {
-  return <div>First Post</div>;
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/* ADDING THIRD-PARTY JS ---------> below is how we could add a third-party javascript into our Next.JS app */}
+      {/* <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      /> */}
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
 }
 
 export default FirstPost;
