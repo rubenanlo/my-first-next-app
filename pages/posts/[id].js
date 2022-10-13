f3336486876e8fea1c061c1da656ccdf9ac5a5b3
Ruben Andino <rubenanlo@gmail.com>
2022-10-13 21:52:54 +0200

    checkpoint

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { getAllPostsIds, getPostData } from "../../lib/posts";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.id}
+      <br />
+      {postData.date} <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
